f0ca6b6398070a1c6879cde3d84724748c261187
Lukas412 <lukas.weber.4.12@gmail.com>
2023-10-01 15:35:11 +0200

    try out xilem

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use xilem::view::{button, h_stack, v_stack, View};
+use xilem::{App, AppLauncher};
+
+fn main() {
+    let app = App::new(AppData {}, app_logic);
+    let launcher = AppLauncher::new(app);
+    launcher.run()
+}
+
+struct AppData {}
+
+fn app_logic(_data: &mut AppData) -> impl View<AppData> {
+    v_stack((
+        button("hello", |_| println!("Hi")),
+        h_stack((
+            button("right", |_| println!("right")),
+            button("wrong", |_| println!("wrong")),
+        )),
+    ))
+}
