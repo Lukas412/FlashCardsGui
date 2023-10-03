4f8e2681053e9d98db359df5082f0e47b0321fbe
lukas <lukas.weber.4.12@gmail.com>
2023-10-03 13:51:54 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,33 @@
-fn main() {}
+use iced::widget::{button, column, text, Column};
+use iced::{Element, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    FlashCards::run(Settings::default())
+}
+
+enum FlashCards {
+    QuestionPage { question: String },
+    AnswerPage { answer: String },
+}
+
+impl Sandbox for FlashCards {
+    type Message = ();
+
+    fn new() -> Self {
+        FlashCards::QuestionPage {
+            question: "Das ist die Fragekarte.".to_owned(),
+        }
+    }
+
+    fn title(&self) -> String {
+        "Karteikarten App (Rust)".to_owned()
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        todo!()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        column![text("hallo"), text("other  text")].into()
+    }
+}
